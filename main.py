755ee5d73d8b73f9053e5f1491de56905a801d44
Edwin <82239374+HAEdwin@users.noreply.github.com>
2021-05-12 00:07:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ class HTTPSERVER(BaseRequestHandler):
     def handle(self):
         rec = self.request.recv(1024)
         if rec:
-            date_time_obj = datetime.strptime(str(rec)[62:76], '%Y%m%d%H%M%S') + timedelta(minutes=-5)
-            send_str = '101' + datetime.strftime(date_time_obj, '%Y%m%d%H%M%S')
-            self.request.send(send_str.encode('utf-8'))
-            
-
+            try:
+                date_time_obj = datetime.strptime(str(rec)[62:76], '%Y%m%d%H%M%S') + timedelta(minutes=-5)
+                send_str = '101' + datetime.strftime(date_time_obj, '%Y%m%d%H%M%S')
+            except:
+                send_str = '100'  # all is fine ;)
+            finally:
+                self.request.send(send_str.encode('utf-8'))
 
 listener_1 = socketserver.TCPServer((host, 8995), HTTPSERVER)
 thread_1 = threading.Thread(target=listener_1.serve_forever)
